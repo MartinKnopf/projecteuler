132c3f2b57b4b44834f53d6d3c7e6027146ae4d2
Martin Knopf <legendaryhorsed@gmail.com>
2019-09-26 09:55:56 +0200

    solved 0002

--- test/0002-even-fibonacci-numbers.js
+++ test/0002-even-fibonacci-numbers.js
@@ -1,14 +1,35 @@
-var assert = require('assert')
+import test from 'ava'
 
-function fib(n) {
-  return [1, 2, 3, 5, 8]
+const fib = (n, numbers) => {
+  let [a, b, ...rest] = [...numbers].reverse()
+  if(a+b < n) return fib(n, [...numbers, a+b])
+  return numbers
 }
 
-describe('[#0002] sum of even fibonacci numbers under 4 million', function() {
+const fibsum = n => {
+  return fib(n, [1, 2]).filter(f => f % 2 == 0).reduce((i, j) => i + j)
+}
 
-  describe('fibonacci sequence', function() {
-    it('should produce fibonacci numbers under 10', function() {
-      assert.equal(fib(0), [1, 2, 3, 5, 8])
-    })
-  })
+test('should produce fibonacci numbers < 10', t => {
+  t.deepEqual(fib(10, [1, 2]), [1, 2, 3, 5, 8])
+})
+
+test('should produce fibonacci numbers < 15', t => {
+  t.deepEqual(fib(15, [1, 2]), [1, 2, 3, 5, 8, 13])
+})
+
+test('should produce fibonacci numbers < 30', t => {
+  t.deepEqual(fib(30, [1, 2]), [1, 2, 3, 5, 8, 13, 21])
+})
+
+test('should return sum of even fibonacci numbers < 10', t => {
+  t.deepEqual(fibsum(10), 10)
+})
+
+test('should return sum of even fibonacci numbers < 40', t => {
+  t.deepEqual(fibsum(40), 44)
+})
+
+test('should return sum of even fibonacci numbers < 4000000', t => {
+  t.deepEqual(fibsum(4000000), 4613732)
 })
